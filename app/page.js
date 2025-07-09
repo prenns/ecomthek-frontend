ad1c5f6c9cc0cc43545aa9825a428d3264b4b5d0
Alexander Prennsberger <a.prennsberger@ecomthek.com>
2025-07-09 12:14:21 +0200

    changed blog text

--- app/page.js
+++ app/page.js
@@ -276,7 +276,7 @@ export default async function Home() {
 
                             <Link href="/notion-templates/marketing-calendar">
                                 <h5 className="mb-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                    Blog & Insights
+                                    Wissen & Lernen
                                 </h5>
                             </Link>
                             <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
